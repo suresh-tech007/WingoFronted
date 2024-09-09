7a19657c081de2362d45d5fac1a8188d165b3017
suresh-tech007 <sk4896323@gmail.com>
2024-09-09 16:10:32 +0530

    add controller for userrefer bonus

--- src/redux/backedUrl.js
+++ src/redux/backedUrl.js
@@ -1,5 +1,5 @@
  
 
-// export const backedurl = "https://reddot-1geh.onrender.com";
-export const backedurl = "https://red-dot-ebon.vercel.app/";
+export const backedurl = "https://reddot-1geh.onrender.com";
+// export const backedurl = "https://red-dot-ebon.vercel.app/";
 // export const backedurl = "http://localhost:4000"; 
